ab78e8a8b701437ad254cf65ad0731f162a259f2
Andy Schwarz <aschwarz@hso.com>
2026-06-15 22:27:20 +0200

    Revert "Add a conversion funnel panel"
    This reverts commit 5e02fb88bced58c7b7dccc051d4aad64f5a3d094.

--- apps/sales-dashboard/src/App.tsx
+++ apps/sales-dashboard/src/App.tsx
@@ -16,7 +16,6 @@ import { Header } from './components/Header'
 import { KpiBar } from './components/KpiBar'
 import { DashboardTile } from './components/DashboardTile'
 import { LoadingOverlay } from './components/LoadingOverlay'
-import { ConversionFunnel } from './components/ConversionFunnel'
 
 /**
  * Moderne Code-App-Fassung des Legacy-Dashboards "Dashboard GVL":
@@ -154,8 +153,6 @@ export default function App() {
           activeTileId={activeTile}
           onSelectTile={setActiveTile}
         />
-
-        <ConversionFunnel data={data} ctx={ctx} />
 
         {activeTile === 'activities' && (
           <DashboardTile def={activitiesTile} rows={data.activities} ctx={ctx} orgUrl={orgUrl} appId={recordLinkAppId} fullWidth />
